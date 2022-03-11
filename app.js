de737e73bd070e3d54fd383c78bf3d65c71edddd
Royadrian32 <rooyespinoza@gmail.com>
2022-03-11 03:44:57 -0300

    servidor funcionando

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const path = require ('path')
+const app = express();
+const port = 3099;
+
+app.use(express.static('public'));
+
+
+app.get('/',(req,res)=>{
+ return res.sendFile(path.resolve(__dirname,'views','index.html'))
+});
+
+app.get('/about',(req,res)=>{
+    return res.sendFile(path.resolve(__dirname,'views','about.html'))
+})
+
+app.get('/music',(req,res)=>{
+    return res.sendFile(path.resolve(__dirname,'views','music.html'))
+})
+
+app.get('/contact',(req,res)=>{
+    return res.sendFile(path.resolve(__dirname,'views','contact.html'))
+})
+
+
+app.get('*',(req,res)=>{
+    return res.sendFile(path.resolve(__dirname,'views','notFound.html'))
+})
+
+
+
+
+
+
+
+
+app.listen(port,() => (console.log(`server running on por http://localhost:${port}`)))
+
